40381ce4d072db8a5bd311ee8c94e909bafde20a
ARotterGoodMan <2195007463@qq.com>
2022-05-07 20:13:25 +0800

    提交
    Signed-off-by: ARotterGoodMan <2195007463@qq.com>

--- download/main.py
+++ download/main.py
@@ -5,5 +5,6 @@ if __name__ == '__main__':
     FilmLibrary = FilmLibrary.FilmLibrary()
     url = input("请输入要拉取的m3u8文件：")
     FilmLibrary.run(url)
+    video_name = input("请输入下载视频的文件名")
     Download = Download.Download()
-    Download.main(url)
+    Download.main(url, video_name)
--- download/src/Download.py
+++ download/src/Download.py
@@ -9,10 +9,12 @@ class Download(object):
     def __init__(self):
         self.download_num = 0
         self.download_over_num = 0
+        self.video_name = None
 
     async def structure_url(self, url):
         tasks, indexes = [], []
         http_top = url
+        http_top_del = http_top.split("/")[-1]
         header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                                 "Chrome/102.0.5005.13 Safari/537.36Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                                 "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.13 Safari/537.36"}
@@ -31,8 +33,8 @@ class Download(object):
                             line = http_top + line
                             line = line.strip()
                         else:
-                            http_top = http_top.split(line.split("/")[0])[0]
-                            line = http_top + line
+                            http_top = http_top.split(f'/{http_top_del}')[0]
+                            line = http_top + '/' + line
                             line = line.strip()
                     indexes.append(line)
                     ts_url = line
@@ -52,6 +54,7 @@ class Download(object):
 
     async def merge_ts(self):
         async with aiofiles.open("video_ts\\merge.bat", mode="a", encoding="utf-8") as merge:
+            await merge.write("chcp 65001\n")
             await merge.write("copy /b ")
             with open("index.m3u8", mode="r", encoding="utf-8") as f:
                 for line in f:
@@ -60,20 +63,21 @@ class Download(object):
                     line = line.strip()
                     file_name = line.split("/")[-1]
                     await merge.write(f'{file_name}+')
-            await merge.write(" movie.mp4")
+            await merge.write(f" {self.video_name}.mp4")
         self.update_merge_bat()
         self.run_bat()
 
     def update_merge_bat(self):
         merge_read = open("video_ts\\merge.bat", mode="r", encoding="utf-8")
-        line2 = merge_read.read().replace("+ movie.mp4", " movie.mp4")
+        line2 = merge_read.read().replace(f"+ {self.video_name}.mp4", f" {self.video_name}.mp4")
         merge_read.close()
         merge_update = open("video_ts\\merge.bat", mode="w", encoding="utf-8")
         merge_update.write(line2)
         merge_update.write('\nmkdir movie\n')
-        merge_update.write('move movie.mp4 movie\n')
+        merge_update.write(f'move {self.video_name}.mp4 movie\n')
         merge_update.close()
-        run_merge = open("run.bat", "w")
+        run_merge = open("run.bat", mode="w", encoding="utf-8")
+        run_merge.write("chcp 65001\n")
         run_merge.write("cd video_ts\n")
         run_merge.write("merge.bat\n")
         run_merge.close()
@@ -84,11 +88,12 @@ class Download(object):
         os.system("del run.bat index.m3u8")
         print("OK!!!!!!!")
 
-    def main(self, url):
+    def main(self, url, video_name):
+        self.video_name = video_name
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.structure_url(url))
         print("视频片段下载完成开始合并视频！！！！")
         time.sleep(2)
-        # loop.run_until_complete(self.merge_ts())
+        loop.run_until_complete(self.merge_ts())
         # asyncio.run(self.structure_url())
         # asyncio.run(self.merge_ts_bat())
