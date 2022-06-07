dd2dbff9812889e5bd76525f86b757254bd4b7fd
ARotterGoodMan <2195007463@qq.com>
2022-06-07 18:05:37 +0800

    优化代码

--- src/selenium_webdriver.py
+++ src/selenium_webdriver.py
@@ -13,30 +13,27 @@ from selenium.webdriver.common.by import By
 
 class SeleniumWebdriver:
     def __init__(self):
-        pass
+        self.school_list = []
+        self.url_list = []
 
-    @staticmethod
-    def get_url():
-        school_list = []
-        url_list = []
+    def get_url(self):
         with open('AcquiredData/school.csv', 'r', encoding='utf-8') as f:
             for line in f.readlines():
                 line = line.strip()
                 school, url = line.split(',')
                 url = "https://gaokao.chsi.com.cn" + url.strip()
-                school_list.append(school)
-                url_list.append(url)
-        return school_list, url_list
+                self.school_list.append(school)
+                self.url_list.append(url)
 
     @staticmethod
-    def write_links(driver, school_list, url_list):
-        driver.get(url_list)
+    def write_links(driver, school, url):
+        driver.get(url)
         links = driver.find_elements(By.XPATH, '/html/body/div[1]/div[4]/div/div/div[2]/table/tbody/tr[1]/td[1]/a')
         with open('AcquiredData/school_links.txt', 'a', encoding='utf-8') as f:
             if len(links) == 0:
                 f.write('')
             else:
-                f.write(school_list + ', ')
+                f.write(school + ', ')
                 for link in links:
                     f.write(link.get_attribute('href') + '\n')
 
@@ -46,6 +43,6 @@ class SeleniumWebdriver:
         chrome_options.add_argument('--disable-gpu')
         driver = webdriver.Chrome(chrome_options=chrome_options)
         # driver = webdriver.Chrome()
-        datas = self.get_url()
-        for i in tqdm(range(0, len(datas[1]))):
-            self.write_links(driver, datas[0][i], datas[1][i])
+        self.get_url()
+        for i in tqdm(range(0, len(self.url_list))):
+            self.write_links(driver, self.school_list[i], self.url_list[i])
